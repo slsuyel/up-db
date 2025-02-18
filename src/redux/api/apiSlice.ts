c54094111eae6845ba33abd529b242f605707735
slsuyel <slsuyel@gmail.com>
2025-02-18 11:34:30 +0600

    fix: change base api

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,20 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const apiSlice = createApi({
-  reducerPath: "api",
+  reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.uniontax.gov.bd/api",
-    credentials: "include",
+    baseUrl: 'https://api.unionservices.gov.bd/api',
+    credentials: 'include',
   }),
   // endpoints: (builder: any) => ({}),
   endpoints: () => ({}),
   tagTypes: [
-    "logout",
-    "profileCreate",
-    "profileUpdate",
-    "sonod-action",
-    "holding",
-    "holding_pay",
+    'logout',
+    'profileCreate',
+    'profileUpdate',
+    'sonod-action',
+    'holding',
+    'holding_pay',
   ],
 });
 
